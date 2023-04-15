523ce43ec835d07c1f4268383b28623df9ba15a2
unknown <bravotech0sa@gmail.com>
2023-04-15 03:49:14 +0300

    upadte

---
+++ 2)AccessTokens Expiration/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsersContoller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+// Public Routes
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+});
+
+// Protected Routes
+Route::middleware('auth:api')->group(function () {
+    Route::get('/users', [UsersContoller::class, 'GetUsers']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
