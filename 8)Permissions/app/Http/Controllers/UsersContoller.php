7d5d922927c14895fbeed77fd8ce8f9aaa6cda69
NageebDarwish <nageeb9696youtub@gmail.com>
2023-06-20 00:26:39 +0300

    update

---
+++ 8)Permissions/app/Http/Controllers/UsersContoller.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UsersContoller extends Controller
+{
+    public function GetUsers()
+    {
+        return User::all();
+    }
+    // Get Auth User
+    public function authUser()
+    {
+        return Auth::user();
+    }
+
+    // Get Specific User
+    public function getUser($id)
+    {
+        return User::findOrFail($id);
+    }
+
+    // Edit User
+    public function editUser(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'role' => 'required',
+        ]);
+        $user = User::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->role = $request->role;
+        $user->save();
+    }
+
+    // Delete User
+    public function destroy($id)
+    {
+        return  User::findOrFail($id)->delete();
+    }
+}
